5eb7950cb0aadf53b1e8257a9a2b6188d0693ed3
ike-jide <ikejideofor@gmail.com>
2023-08-02 21:16:38 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './app.css'
+
+function App() {
+  const moneyPyramid =[
+    {id:1,amount:"$ 100"},
+    {id:2,amount:"$ 200"},
+    {id:3,amount:"$ 300"},
+    {id:4,amount:"$ 500"},
+    {id:5,amount:"$ 1000"},
+    {id:6,amount:"$ 2000"},
+    {id:7,amount:"$ 4000"},
+    {id:8,amount:"$ 8000"},
+    {id:9,amount:"$ 15000"},
+    {id:10,amount:"$ 30000"},
+    {id:11,amount:"$ 60000"},
+    {id:12,amount:"$ 100000"},
+    {id:13,amount:"$ 200000"},
+    {id:14,amount:"$ 500000"},
+    {id:15,amount:"$ 1000000"}
+  ].reverse()
+  return (
+  <div className="app">
+<div className="main"></div>
+<div className='side'>
+  <ul className='moneyList'>
+    {moneyPyramid.map((m)=>(
+    <li className='moneyListItem'>
+    <span className='moneyListNumber'>{m.id}</span>
+    <span className='moneyListAmount'>{m.amount}</span>
+  </li>
+    ))}
+    
+    
+  </ul>
+</div>
+  </div>
+  );
+}
+
+export default App;
