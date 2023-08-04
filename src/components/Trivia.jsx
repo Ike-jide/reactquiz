d11aeba7272496db5311f86960c5c5f10a5da580
ike-jide <ikejideofor@gmail.com>
2023-08-04 16:43:13 +0100

    Added new animations and more reactivity

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 
-export default function Trivia(){
+export default function Trivia({data,
+    setQuestionNumber,
+    setTimeOut,
+    questionNumber}){
+        useEffect(()=>{
+            setQuestion(data[questionNumber - 1]);
+
+        },
+        [data,questionNumber]
+        )
     return(
         <div className="trivia">
       <div className="question">
         food whats the best
       </div>
       <div className="answers">
-      <div className="answer">
+      <div className="answer correct">
         the goat
       </div>
       <div className="answer">
