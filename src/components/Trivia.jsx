2b773c6134ad5fdc498c9f4bb160ad3d3ba7fd5d
ike-jide <ikejideofor@gmail.com>
2023-08-05 17:29:08 +0100

    more fumctionallity added

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,34 +1,36 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 
 export default function Trivia({data,
     setQuestionNumber,
     setTimeOut,
     questionNumber}){
+        const [question,setQuestion]= useState(null);
+        const [selectedAnswer,setSelectedAnswer]= useState(null);
+        const [className,setClassName]= useState('answer');
         useEffect(()=>{
             setQuestion(data[questionNumber - 1]);
 
         },
         [data,questionNumber]
-        )
+        );
+        const handleClick = (a)=>{
+            setSelectedAnswer(a)
+            setClassName('answer active')
+        } 
     return(
         <div className="trivia">
       <div className="question">
-        food whats the best
+        {question?.question}
       </div>
       <div className="answers">
-      <div className="answer correct">
-        the goat
+        {question?.answers.map((a)=>(
+       < div className={selectedAnswer === a ? className : "answer"} onClick={()=>handleClick(a)}>
+        {a.text}
       </div>
-      <div className="answer">
-        the goat
-      </div>
-      <div className="answer">
-        the goat
-      </div>
-      <div className="answer">
-        the goat
-      </div>
+       )  )}
+      
+      
       </div>
         </div>
     )
